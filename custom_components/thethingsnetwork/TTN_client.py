ec79bb0633a8ee5410f5896939a0b243bffc0dcd
Vegetto <git@angelnu.com>
2020-09-04 02:46:42 +0200

    remove new claases

--- custom_components/thethingsnetwork/TTN_client.py
+++ custom_components/thethingsnetwork/TTN_client.py
@@ -4,11 +4,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers import device_registry as dr
 from homeassistant.const import CONTENT_TYPE_JSON, HTTP_NOT_FOUND
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
-from homeassistant.helpers.update_coordinator import (
-    CoordinatorEntity,
-    DataUpdateCoordinator,
-    UpdateFailed,
-)
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
 import asyncio
 import aiohttp
